178f55b56a321b3e5e17e6449346192156bc08fb
luomiao3 <675308992@qq.com>
2015-12-02 16:49:26 +0800

    双人模式 + 围棋规则开发中...

---
+++ public/js/weiqiGame.js
@@ -0,0 +1,7 @@
+/**
+ * weiqiGame
+ * Authors: Michael Luo
+ * Date: 15/11/22
+ * Copyright (c) 2015 Swarma Wiki. All rights reserved.
+ * http://wiki.swarma.net/index.php/%E7%94%9F%E5%91%BD%E6%B8%B8%E6%88%8F%E5%9B%B4%E6%A3%8B
+ */
