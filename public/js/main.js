e353678f369111afceedc191785e8b25d7200f1d
bitwater <675308992@qq.com>
2016-05-26 00:14:33 +0800

    dev dropdown-menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,27 @@
 requirejs.config({
   baseUrl: '/js',
   paths: {
-    "jquery": "//cdn.bootcss.com/jquery/1.11.3/jquery.min",
-    "bootstrap": "//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min"
-    //"jquery": "lib/jquery.min",
-    //"bootstrap": "lib/bootstrap.min"
+    //"jquery": "//cdn.bootcss.com/jquery/1.11.3/jquery.min",
+    //"bootstrap": "//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min"
+    "jquery": "lib/jquery.min",
+    "bootstrap": "lib/bootstrap.min"
   },
   shim: {
-    "bootstrap": ["jquery"]
+    "bootstrap": {
+      deps:['jquery']
+    }
+      //["jquery"]
   }
 });
 
-requirejs(['app', 'config', 'jquery', 'roomApp'], function(App, Config, $, roomApp) {
+requirejs(['app', 'config', 'jquery', 'roomApp', 'bootstrap'], function(App, Config, $, roomApp, bootstrap) {
   if($('#room').length) {
     var app = new roomApp(Config);
   } else {
     var app = new App(Config);
   }
 
+  console.log($, bootstrap);
   app.init();
   window.app = app;
 });
