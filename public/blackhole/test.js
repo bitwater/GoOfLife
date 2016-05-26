617339f4b791f3b9f208e57c1f08904cc4161524
bitwater <675308992@qq.com>
2016-05-26 21:15:49 +0800

    add blackhole

--- public/blackhole/test.js
+++
@@ -1,7 +0,0 @@
-/**
- * test.js
- * Authors: Michael Luo
- * Date: 16/5/25
- * Copyright (c) 2015 Swarma Wiki. All rights reserved.
- * http://wiki.swarma.net/index.php/%E7%94%9F%E5%91%BD%E6%B8%B8%E6%88%8F%E5%9B%B4%E6%A3%8B
- */
