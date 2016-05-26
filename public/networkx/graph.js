98936ffb489c6814acc7d516f7a253c505fe07fd
bitwater <675308992@qq.com>
2016-05-26 15:33:28 +0800

    dev networkx

--- public/networkx/graph.js
+++ public/networkx/graph.js
@@ -4,29 +4,96 @@ $(function() {
   "use strict";
   var G;
   var tick;
+
+  $(function () {
+    var grid = jsnx.grid2dGraph(6, 6);
+    var color = d3.scale.category10();
+    d3.select('#canvas4').style('height', '400px');
+    //d3.select('#canvas-bg').style('width', '100%');
+
+    jsnx.draw(grid, {
+      element: '#canvas4',
+      layoutAttr: {
+        charge: -300,
+        linkDistance: 25,
+        gravity: 0.5
+      },
+      nodeAttr: {
+        r: 5,
+        title: function (d) {
+          return d.label;
+        }
+      },
+      nodeStyle: {
+        fill: function (d) {
+          return color(d.node % 4);
+        },
+        stroke: 'none'
+      },
+      edgeStyle: {
+        fill: '#999'
+      }
+    });
+
+    var G1 = jsnx.completeGraph(random(3, 6));
+    var G2 = new jsnx.Graph();
+
+    G2.addNodesFrom([1, 2, 3, 4], {group: 0});
+    G2.addNodesFrom([5, 6, 7], {group: 1});
+    G2.addNodesFrom([8, 9, 10, 11], {group: 2});
+    G2.addPath([1, 2, 5, 6, 7, 8, 11]);
+    G2.addEdgesFrom([[1, 3], [1, 4], [3, 4], [2, 3], [2, 4], [8, 9], [8, 10], [9, 10], [11, 10], [11, 9]]);
+    var color = d3.scale.category20();
+    jsnx.draw(G2, {
+      element: '#canvas2',
+      layoutAttr: {
+        charge: -120,
+        linkDistance: 20
+      },
+      nodeAttr: {
+        r: 5,
+        title: function (d) {
+          return d.label;
+        }
+      },
+      nodeStyle: {
+        fill: function (d) {
+          return color(d.data.group);
+        },
+        stroke: 'none'
+      },
+      edgeStyle: {
+        fill: '#999'
+      }
+    });
+  })
+
+  function random(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+  }
+
   function draw() {
     clearTimeout(tick);
-    var color = d3.scale.category20();
-    if (Math.random() <= 0.02) {
-      // NetworkX graph
-      G = new jsnx.Graph();
-      G.addNodesFrom([1,2,3,4], {group:1});
-      G.addNodesFrom([5,6,7], {group:2});
-      G.addNodesFrom([8,9,10,11], {group:3});
-
-      G.addPath([1,2,5,6,7,8,11]);
-      G.addEdgesFrom([
-                       [1,3],[1,4],[3,4],[2,3],[2,4],[8,9],[8,10],[9,10],[11,10],[11,9]
-      ]);
-    }
-    else {
-      color = d3.scale.category10();
+    var color = d3.scale.category10();
+    //if (Math.random() <= 0.02) {
+    //  // NetworkX graph
+    //  G = new jsnx.Graph();
+    //  G.addNodesFrom([1,2,3,4], {group:1});
+    //  G.addNodesFrom([5,6,7], {group:2});
+    //  G.addNodesFrom([8,9,10,11], {group:3});
+    //
+    //  G.addPath([1,2,5,6,7,8,11]);
+    //  G.addEdgesFrom([
+    //                   [1,3],[1,4],[3,4],[2,3],[2,4],[8,9],[8,10],[9,10],[11,10],[11,9]
+    //  ]);
+    //}
+      //color = d3.scale.category10();
       // var edges = jsnx.binomialGraph(
         // Math.floor((Math.random() * 11) + 10),
         // 0.12
       // ).edges();
       // var edges = jsnx.binomialGraph(4, 0.5).edges();
-      var edges = jsnx.fastGnpRandomGraph(33, 0.1).edges();
+      var edges = jsnx.fastGnpRandomGraph(23, 0.1).edges();
       // var edges = jsnx.completeGraph(6).edges();
 
       G = new jsnx.Graph();
@@ -41,17 +108,15 @@ $(function() {
           G.addEdge(2, 10);
         }
       }());
-    }
 
-    d3.select('#canvas3').style('opacity', 0.01)
-    .transition().style('opacity', 1);
+    d3.select('#canvas3').style('opacity', 0.01).transition().style('opacity', 1);
     d3.select('#canvas3').style('height', '400px');
 
     jsnx.draw(G, {element: '#canvas3',
               layoutAttr: {
                 // width: 400,
-                charge: -100,
-                linkDistance: 20,
+                charge: -300,
+                linkDistance: 30,
                 gravity: 0.3
               },
               // panZoom: {
